477bdace17089ca2dc15ede42794e499d3ca0bdc
rlynn523 <ryanlynn523@gmail.com>
2016-06-03 18:02:24 -0400

    blur function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".quiz, .ratings, .shopping-list").mouseover(function(){
-		$(this).css({'filter':'blur(2px)', '-webkit-filter': 'blur(2px)'});
+		$(this).css({'opacity':'blur(2px)', '-webkit-filter': 'blur(2px)'});
 	})
 	.mouseleave(function(){
 		$(this).css({'filter':'blur(0px)', '-webkit-filter': 'blur(0px)'});
