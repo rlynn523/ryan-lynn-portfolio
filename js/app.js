4ca3aa37275bde94135509354299e2824b61ed21
rlynn523 <ryanlynn523@gmail.com>
2016-06-03 20:33:53 -0400

    added links to projects and text div that appears when mouseenter/mouseleave

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 $(function(){
-	$(".quiz, .ratings, .shopping-list").mouseover(function(){
-		$(this).css({'opacity':'blur(2px)', '-webkit-filter': 'blur(2px)'});
+	$(".quiz").mouseover(function(){
+		$(this).css({'filter':'blur(4px)', '-webkit-filter': 'blur(4px)'});
+		$(".quiz-text").show();
 	})
 	.mouseleave(function(){
 		$(this).css({'filter':'blur(0px)', '-webkit-filter': 'blur(0px)'});
+		$(".quiz-text").hide();
+	})
+	$(".ratings").mouseover(function(){
+		$(this).css({'filter':'blur(4px)', '-webkit-filter': 'blur(4px)'});
+		$(".ratings-text").show();
+	})
+	.mouseleave(function(){
+		$(this).css({'filter':'blur(0px)', '-webkit-filter': 'blur(0px)'});
+		$(".ratings-text").hide();
+	})
+	$(".shopping-list").mouseover(function(){
+		$(this).css({'filter':'blur(4px)', '-webkit-filter': 'blur(4px)'});
+		$(".list-text").show();
+	})
+	.mouseleave(function(){
+		$(this).css({'filter':'blur(0px)', '-webkit-filter': 'blur(0px)'});
+		$(".list-text").hide();
 	})
 });
